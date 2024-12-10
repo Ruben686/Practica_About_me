a3f22aaa13fcedb24db92322fb954d39770f5400
RubenMunoz <ruben.munoz@gracia.lasalle.cat>
2024-12-10 12:54:58 +0100

    Métodos getter para obtener cada atributo del perfil, además de asignar los atributos.

--- src/MunozRubenpractica2ej1.java
+++ src/MunozRubenpractica2ej1.java
@@ -8,4 +8,33 @@ public class MunozRubenpractica2ej1 {
     private String[] hobbies; // Array de hobbies ya que al contrario que los otros habrá mas de 1
     private String[] foods; //Array comidas
     private String[] funFacts;//Array datos curiosos
+
+    public MunozRubenpractica2ej1(String name, String story, String[] hobbies, String[] foods, String[] funFacts) {
+        this.name = name;         // Asigna el nombre al atributo
+        this.story = story;       // Asigna la historia al atributo
+        this.hobbies = hobbies;   // Asigna los hobbies
+        this.foods = foods;       // Asigna las comidas favoritas
+        this.funFacts = funFacts; // Asigna los datos curiosos
+    }
+    // Métodos getter para obtener cada atributo del perfil.
+    public String getName() {
+        return name;
+    }
+
+    public String getStory() {
+        return story;
+    }
+
+    public String[] getHobbies() {
+        return hobbies;
+    }
+
+    public String[] getFoods() {
+        return foods;
+    }
+
+    public String[] getFunFacts() {
+        return funFacts;
+    }
+
 }
