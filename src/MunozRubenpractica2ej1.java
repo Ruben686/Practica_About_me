573e7a2bf37f7ffdde34cc9bea05d7e22c59bb34
RubenMunoz <ruben.munoz@gracia.lasalle.cat>
2024-12-10 12:47:38 +0100

    Creación atributosprivados

---
+++ src/MunozRubenpractica2ej1.java
@@ -0,0 +1,11 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
+public class MunozRubenpractica2ej1 {
+    //Creamos atributos privados para mantener la integridad de la clase
+    private String name;//nombre del perfil
+    private String story;//historia del perfil
+    private String[] hobbies; // Array de hobbies ya que al contrario que los otros habrá mas de 1
+    private String[] foods; //Array comidas
+    private String[] funFacts;//Array datos curiosos
+}
